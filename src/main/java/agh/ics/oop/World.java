2030ab5a26b4776ea9b1f17110381eda0507f599
tkajda <tomaszkajda4@gmail.com>
2021-10-21 14:18:35 +0200

    lab2

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -17,7 +17,7 @@ public class World {
 
 
         Vector2d position1 = new Vector2d(1,2);
-        System.out.println(position1);
+        System.out.println(position1.x + position1.y);
         Vector2d position2 = new Vector2d(-2,1);
         System.out.println(position2);
         System.out.println(position1.add(position2));
