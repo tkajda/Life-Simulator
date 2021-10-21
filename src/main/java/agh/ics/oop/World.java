16cfa8ce77d64ad694a47e6a450a806a2422ddd3
kajda <kajda@d17-429-pc-07.ipa.ki.agh.edu.pl>
2021-10-21 13:31:57 +0200

    Initial commit

---
+++ src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,55 @@
+package agh.ics.oop;
+
+
+public class World {
+
+    public static void main(String[] args) {
+
+        int cnt =0;
+        System.out.print("Start\n");
+        for(String i: args) {
+            if(cnt<args.length-1) {
+                System.out.print(i + ',');
+            }
+            cnt++;
+        }
+        System.out.println(args[args.length-1]);
+
+        Direction[] arr = change(args);
+        run(arr);
+        System.out.print("Stop\n");
+    }
+
+    public static void run(Direction[] args) {
+        for(int i = 0; i < args.length;i++) {
+
+            if (args[i] != null) {
+                switch (args[i]) {
+                    case FORWARD -> System.out.println("Zwierzak idzie do przodu");
+                    case BACKWARD-> System.out.println("Zwierzak idzie do tyłu");
+                    case RIGHT -> System.out.println("Zwierzak skręca w prawo");
+                    case LEFT -> System.out.println("Zwierzak skręca w lewo");
+                }
+            }
+        }
+    }
+
+
+    public static Direction[] change(String[] args){
+        Direction[] enumargs = new Direction[args.length];
+        int cnt=0;
+        for(int i =0; i<args.length;i++) {
+            enumargs[i] = switch(args[i]) {
+                case "f" -> Direction.FORWARD;
+                case "b" -> Direction.BACKWARD;
+                case "r" -> Direction.RIGHT;
+                case "l" -> Direction.LEFT;
+                default -> null;
+
+            };
+        }
+        return enumargs;
+    }
+
+
+}
